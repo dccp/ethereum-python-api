97875e9505a7e2281cbe2c7e0af3bc9e3d0d200b
Oskar Nyberg <oskar@oskarnyberg.com>
2015-02-20 11:17:10 +0100

    Created coinbase method and helper functions

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-import json
+import json, requests
 
+def getRPCId():
+    return 1
+
+def sendJSONRequest(method, params):
+    data = json.dumps({"jsonrpc":"2.0", "method":method, "params":params, "id":getRPCId()})
+    response = requests.post("http://localhost:1337", data=data)
+    return response.json()
+
+def eth_coinbase():
+    return sendJSONRequest("eth_coinbase", [])
 
