9e4fa6b1aae7e559a9ce2e80190945e3023fad8b
Oskar Nyberg <oskar@oskarnyberg.com>
2015-02-20 11:25:29 +0100

    Implemented setCoinbase method

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ def getRPCId():
 def sendJSONRequest(method, params):
     data = json.dumps({"jsonrpc":"2.0", "method":method, "params":params, "id":getRPCId()})
     response = requests.post("http://localhost:1337", data=data)
-    return response.json()
+    return response.json()['result']
 
 def eth_coinbase():
     return sendJSONRequest("eth_coinbase", [])
 
+def eth_setCoinbase(coinbase): # Not implemented 150220
+    return sendJSONRequest("eth_setCoinbase", [coinbase])
+
+print eth_coinbase()
