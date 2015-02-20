1c8ed2cba4cf1dad31b59a428b118bedc6d43e5d
Oskar Nyberg <oskar@oskarnyberg.com>
2015-02-20 13:34:24 +0100

    Improved sendJSONRequest

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ def getRPCId():
     return 1
 
 def sendJSONRequest(method, *params):
-    if len(params) == 0:
-        params = []
-    else:
-        params = params[0]
-
     data = json.dumps({"jsonrpc":"2.0", "method":method, "params":params, "id":getRPCId()})
     response = requests.post("http://localhost:1337", data=data)
     jsondata = response.json()
@@ -23,7 +18,7 @@ def eth_coinbase():
 # Not implemented 150220 in go-ethereum
 # Implemented in cpp-ethereum
 def eth_setCoinbase(coinbase):
-    return sendJSONRequest("eth_setCoinbase", [coinbase])
+    return sendJSONRequest("eth_setCoinbase", coinbase)
 
 def eth_listening():
     return sendJSONRequest("eth_listening")
@@ -31,7 +26,7 @@ def eth_listening():
 # Not implemented 150220 in go-ethereum
 # Implemented in cpp-ethereum
 def eth_setListening(listening):
-    return sendJSONRequest("eth_setListening", [listening])
+    return sendJSONRequest("eth_setListening", listening)
 
 def eth_mining():
     return sendJSONRequest("eth_mining")
@@ -39,11 +34,11 @@ def eth_mining():
 # Not implemented 150220 in go-ethereum
 # Implemented in cpp-ethereum
 def eth_setMining(mining):
-    return sendJSONRequest("eth_setMining", [mining])
+    return sendJSONRequest("eth_setMining", mining)
 
 def eth_gasPrice():
     return sendJSONRequest("eth_gasPrice")
 
 def eth_accounts(client=None):
-    return sendJSONRequest("eth_accounts", [client])
+    return sendJSONRequest("eth_accounts", client)
 
