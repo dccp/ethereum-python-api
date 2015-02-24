cf5ad2818ed678d3b9fc508f9875af7222360083
Oskar Nyberg <oskar@oskarnyberg.com>
2015-02-24 11:49:39 +0100

    Fixed errors

--- ethereumAPI.py
+++ ethereumAPI.py
@@ -6,144 +6,144 @@ class EthereumAPI:
         self.json = ethereumJSON.EthereumJSON()
 
     def coinbase(self):
-        return self.json._sendJSONRequest("coinbase")
+        return self.json._sendJSONRequest("eth_coinbase")
 
     # Not implemented 150220 in go-ethereum
     # Implemented in cpp-ethereum
     def setCoinbase(self, coinbase):
-        return self._sendJSONRequest("setCoinbase", coinbase)
+        return self.json._sendJSONRequest("eth_setCoinbase", coinbase)
 
     def listening(self):
-        return self._sendJSONRequest("listening")
+        return self.json._sendJSONRequest("eth_listening")
 
     # Not implemented 150220 in go-ethereum
     # Implemented in cpp-ethereum
     def setListening(self, listening):
-        return self._sendJSONRequest("setListening", listening)
+        return self.json._sendJSONRequest("eth_setListening", listening)
 
     def mining(self):
-        return self._sendJSONRequest("mining")
+        return self.json._sendJSONRequest("eth_mining")
 
     # Not implemented 150220 in go-ethereum
     # Implemented in cpp-ethereum
     def setMining(self, mining):
-        return self._sendJSONRequest("setMining", mining)
+        return self.json._sendJSONRequest("eth_setMining", mining)
 
     def gasPrice(self):
-        return self._sendJSONRequest("gasPrice")
+        return self.json._sendJSONRequest("eth_gasPrice")
 
     def accounts(self, client=None):
-        return self._sendJSONRequest("accounts", client)
+        return self.json._sendJSONRequest("eth_accounts", client)
 
     def peerCount(self):
-        return self._sendJSONRequest("peerCount")
+        return self.json._sendJSONRequest("eth_peerCount")
 
     def defaultBlock(self):
-        return self._sendJSONRequest("defaultBlock")
+        return self.json._sendJSONRequest("eth_defaultBlock")
 
     def setDefaultBlock(self, block):
-        return self._sendJSONRequest("setDefaultBlock", block)
+        return self.json._sendJSONRequest("eth_setDefaultBlock", block)
 
     def number(self):
-        return self._sendJSONRequest("number")
+        return self.json._sendJSONRequest("eth_number")
 
     def balanceAt(self, address):
-        return self._sendJSONRequest("balanceAt", address)
+        return self.json._sendJSONRequest("eth_balanceAt", address)
 
     # Not working :( TODO:
     def stateAt(self, address, index):
-        return self._sendJSONRequest("stateAt", address, index)
+        return self.json._sendJSONRequest("eth_stateAt", address, index)
 
     def storageAt(self, address):
-        return self._sendJSONRequest("storageAt", address)
+        return self.json._sendJSONRequest("eth_storageAt", address)
 
     def countAt(self, address):
-        return self._sendJSONRequest("countAt", address)
+        return self.json._sendJSONRequest("eth_countAt", address)
 
     def transactionCountByHash(self, hash):
-        return self._sendJSONRequest("transactionCountByHash", hash)
+        return self.json._sendJSONRequest("eth_transactionCountByHash", hash)
 
     def transactionCountByNumber(self, number):
-        return self._sendJSONRequest("transactionCountByNumber", number)
+        return self.json._sendJSONRequest("eth_transactionCountByNumber", number)
 
     def uncleCountByHash(self, hash):
-        return self._sendJSONRequest("uncleCountByHash", hash)
+        return self.json._sendJSONRequest("eth_uncleCountByHash", hash)
 
     def uncleCountByNumber(self, number):
-        return self._sendJSONRequest("uncleCountByNumber", number)
+        return self.json._sendJSONRequest("eth_uncleCountByNumber", number)
 
     def codeAt(self, address):
-        return self._sendJSONRequest("codeAt", address)
+        return self.json._sendJSONRequest("eth_codeAt", address)
 
     # TODO: test
     def transact(self, code):
-        return self._sendJSONRequest("transact", json.dumps({"code":code}))
+        return self.json._sendJSONRequest("eth_transact", json.dumps({"code":code}))
 
     # TODO: test
     def call(self, to, data):
-        return self._sendJSONRequest("call", json.dumps({"to":to, "data":data}))
+        return self.json._sendJSONRequest("eth_call", json.dumps({"to":to, "data":data}))
 
     def flush(self):
-        return self._sendJSONRequest("flush")
+        return self.json._sendJSONRequest("eth_flush")
 
     # TODO: Not working :(
     def blockByHash(self, hash):
-        return self._sendJSONRequest("blockByHash", hash)
+        return self.json._sendJSONRequest("eth_blockByHash", hash)
 
     def blockByNumber(self, number):
-        return self._sendJSONRequest("blockByNumber", number)
+        return self.json._sendJSONRequest("eth_blockByNumber", number)
 
     # TODO: test
     def transactionByHash(self, hash, index):
-        return self._sendJSONRequest("transactionByHash", hash, index)
+        return self.json._sendJSONRequest("eth_transactionByHash", hash, index)
 
     # Exception?
     def transactionByNumber(self, number, index):
-        return self._sendJSONRequest("transactionByNumber", number, index)
+        return self.json._sendJSONRequest("eth_transactionByNumber", number, index)
 
     # TODO: test
     def uncleByHash(self, hash, index):
-        return self._sendJSONRequest("uncleByHash", hash, index)
+        return self.json._sendJSONRequest("eth_uncleByHash", hash, index)
 
     def uncleByNumber(self, number, index):
-        return self._sendJSONRequest("uncleByNumber", number, index)
+        return self.json._sendJSONRequest("eth_uncleByNumber", number, index)
 
     def compilers(self):
-        return self._sendJSONRequest("compilers")
+        return self.json._sendJSONRequest("eth_compilers")
 
     # TODO: def lll():
 
     def solidity(self, contract):
-        return self._sendJSONRequest("solidity", contract)
+        return self.json._sendJSONRequest("eth_solidity", contract)
 
     # TODO: test when implemented in ethereum
     #def serpent(contract):
-    #    return self._sendJSONRequest("serpent", contract)
+    #    return self.json._sendJSONRequest("eth_serpent", contract)
 
     # TODO: test
     def newFilter(self, topic):
-        return self._sendJSONRequest("newFilter", json.dumps({"topic":topic}))
+        return self.json._sendJSONRequest("eth_newFilter", json.dumps({"topic":topic}))
 
     # TODO: test
     def newFilterString(self, string):
-        return self._sendJSONRequest("newFilterString", string)
+        return self.json._sendJSONRequest("eth_newFilterString", string)
 
     # TODO: test
     def uninstallFilter(self, id):
-        return self._sendJSONRequest("uninstallFilter", id)
+        return self.json._sendJSONRequest("eth_uninstallFilter", id)
 
     def changed(self, id):
-        return self._sendJSONRequest("changed", id)
+        return self.json._sendJSONRequest("eth_changed", id)
 
     def filterLogs(self, id):
-        return self._sendJSONRequest("filterLogs", id)
+        return self.json._sendJSONRequest("eth_filterLogs", id)
 
     def logs(self, topic):
-        return self._sendJSONRequest("logs", json.dumps({"topic":topic}))
+        return self.json._sendJSONRequest("eth_logs", json.dumps({"topic":topic}))
 
     def getWork(self):
-        return self._sendJSONRequest("getWork")
+        return self.json._sendJSONRequest("eth_getWork")
 
     def submitWork(self, work):
-        return self._sendJSONRequest("submitWork", work)
+        return self.json._sendJSONRequest("eth_submitWork", work)
 
