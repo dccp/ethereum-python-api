99d49917ddea3b621c079b82f3a3e1d5fe4ac5db
Oskar Nyberg <oskar@oskarnyberg.com>
2015-02-24 11:27:55 +0100

    Refactored json stuff into seperate file

---
+++ ethereumJSON.py
@@ -0,0 +1,20 @@
+import json, requests
+
+class EthereumJSON:
+
+    def __init__(self):
+        self.id = 0
+
+    # TODO: Make smarter id generator
+    def _getId(self):
+        self.id += 1
+        return self.id
+
+    def _sendJSONRequest(self, method, *params):
+        data = json.dumps({"jsonrpc":"2.0", "method":method, "params":params, "id":self._getId()})
+        response = requests.post("http://localhost:1337", data=data)
+        jsondata = response.json()
+        if 'result' in jsondata:
+            return jsondata['result']
+        else:
+            return jsondata['error']
